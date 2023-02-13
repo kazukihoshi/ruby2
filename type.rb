4243b518280e7edbc1837e448908f977060ed853
kazukihoshi <starrysky.blue30@gmail.com>
2023-02-13 04:51:27 +0000

    [add]ruby学習

---
+++ type.rb
@@ -0,0 +1,21 @@
+puts 5 + 3
+puts "5+3"
+puts "5" + "3"
+
+puts "I" + "am" + "Sam"
+
+puts "Samの年齢は" + 27.to_s + "です。"
+
+puts 100 + "200".to_i
+
+webcamp = "プログラミング学習"
+puts webcamp
+
+webcamp = "オンラインプログラミング学習"
+puts webcamp
+
+Pi = 3.14
+puts Pi
+
+Pi = 100
+puts Pi
